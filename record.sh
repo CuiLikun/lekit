93d44c1cf77b43a08c79678771c41e4bd2786cba
sorel <sorel.ferris@foxmail.com>
2026-08-10 15:01:11 +0800

    feat: Add recording script for Isaac Teleop to Jaka integration

---
+++ record.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+    # --robot.cameras="{ hand: {type: intelrealsense, serial_number_or_name: '342522070741', width: 640, height: 480, fps: 30}}" \
+
+python -m examples.isaac_teleop_to_jaka.record \
+    --robot.type=jaka_robot \
+    --robot.ip=192.168.1.31 \
+    --robot.id=jaka_arm \
+    --robot.servo_step_num=4 \
+    --teleop.type=xr_controller \
+    --teleop.lock_pose=True \
+    --dataset.repo_id="sorel/pick-cube" \
+    --dataset.single_task="Pick up the object" \
+    --dataset.fps=30 \
+    --dataset.num_episodes=3 \
+    --dataset.episode_time_s=9999 \
+    --dataset.reset_time_s=5 \
+    --dataset.streaming_encoding=True \
+    --dataset.push_to_hub=False
